1b6d8f8121dda63cdf6e0f63915e254a010d5a94
Moshe <shootermv@gmail.com>
2016-10-27 14:01:42 +0300

    view2 moved to typescript

---
+++ app/view2/view2.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../typings/main/ambient/jquery/index.d.ts" />
+/// <reference path="../../typings/main/ambient/angular/index.d.ts" />
+/// <reference path="../../typings/main/ambient/angular-route/index.d.ts" />
+angular.module('myApp.view2', ['ngRoute'])
+
+.config(['$routeProvider', ($routeProvider: angular.route.IRouteProvider) => {
+  $routeProvider.when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl'
+  });
+}])
+
+.controller('View2Ctrl', [() => {
+
+}]); 
