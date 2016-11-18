1ce38f0fd64cd4290eb2763cec472ec9874dde9a
shootermv <shootermv@gmail.com>
2016-11-18 09:31:13 +0200

    include webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var webpack = require('webpack');
+var path = require('path');
+module.exports = {
+    context: path.join( __dirname, '/app'),
+    entry: 'app.ts',
+    output: {
+        filename: 'app.js',
+        path: path.join( __dirname, '/app')
+    },
+    resolve: {
+        root: path.join( __dirname, '/app'),
+        extensions: ['', '.ts', '.webpack.js', '.web.js', '.js']
+    },
+    module: {
+        loaders: [
+        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.ts?$/, loader: 'ts-loader' }
+        ]
+    }
+}
