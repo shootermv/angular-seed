21d577cd6dc1d40c18b63389878d25bdcff2bb16
shootermv <shootermv@gmail.com>
2016-12-26 22:56:10 +0200

    config js no more needed

--- app/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function config($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
-    $routeProvider.otherwise({ redirectTo: '/view1' });
-}
-exports.config = config;
-;
